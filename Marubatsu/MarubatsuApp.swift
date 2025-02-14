1445575b82bc3bdaf718d0bba4dc989c97f7c981
eisan2210 <eisan2210@gmail.com>
2025-02-15 08:35:33 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by satoeisuke on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
